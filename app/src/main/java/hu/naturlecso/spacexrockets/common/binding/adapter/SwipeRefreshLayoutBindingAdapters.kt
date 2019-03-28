aa00c99c23091ed112d64e86cb596b145f069618
Jozsef Szilvasi <jozsef.szilvasi@gmail.com>
2019-03-28 23:48:58 +0100

    Add refresh and filter to rockets

---
+++ app/src/main/java/hu/naturlecso/spacexrockets/common/binding/adapter/SwipeRefreshLayoutBindingAdapters.kt
@@ -0,0 +1,23 @@
+package hu.naturlecso.spacexrockets.common.binding.adapter
+
+import androidx.databinding.BindingAdapter
+import androidx.databinding.BindingMethod
+import androidx.databinding.BindingMethods
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import hu.naturlecso.spacexrockets.common.binding.Command
+
+@BindingMethods(
+    BindingMethod(
+            type = SwipeRefreshLayout::class,
+            attribute = "refreshing",
+            method = "setRefreshing")
+)
+class SwipeAdapterBindingMethods
+
+@BindingAdapter("refreshCommand")
+fun SwipeRefreshLayout.bindRefreshListener(command: Command?) {
+
+    if (command != null) {
+        setOnRefreshListener { command.execute() }
+    }
+}
--- app/src/main/java/hu/naturlecso/spacexrockets/ui/rockets/RocketsViewModel.kt
+++ app/src/main/java/hu/naturlecso/spacexrockets/ui/rockets/RocketsViewModel.kt
@@ -1,20 +1,44 @@
 package hu.naturlecso.spacexrockets.ui.rockets
 
+import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
 import hu.naturlecso.spacexrockets.common.binding.RxObservableField
 import hu.naturlecso.spacexrockets.common.binding.command
 import hu.naturlecso.spacexrockets.common.navigation.Navigator
+import hu.naturlecso.spacexrockets.domain.LaunchAction
 import hu.naturlecso.spacexrockets.domain.Rocket
 import hu.naturlecso.spacexrockets.domain.RocketAction
 import hu.naturlecso.spacexrockets.domain.RocketStore
+import io.reactivex.Flowable
+import io.reactivex.functions.BiFunction
+import io.reactivex.processors.BehaviorProcessor
 
 class RocketsViewModel(
         private val navigator: Navigator,
         private val rocketStore: RocketStore,
-        private val rocketAction: RocketAction
+        private val rocketAction: RocketAction,
+        private val launchAction: LaunchAction
 ) : ViewModel() {
 
-    val rockets = RxObservableField(rocketStore.getList())
+    private val filterInactiveRocketsProcessor = BehaviorProcessor.createDefault<Boolean>(false)
+
+    val rockets = RxObservableField(Flowable.combineLatest(rocketStore.getList(), filterInactiveRocketsProcessor,
+            BiFunction<List<Rocket>, Boolean, List<Rocket>> { rockets, filter ->
+                rockets.filter { it.active || !filter }
+            })
+    )
+
+    val refreshing = ObservableField<Boolean>(false)
+
+    val inactiveRocketsFiltered = RxObservableField<Boolean>(filterInactiveRocketsProcessor)
+
+    val refreshCommand = command {
+        rocketAction.refresh()
+            .andThen(launchAction.refresh())
+            .doOnSubscribe { refreshing.set(true) }
+            .doOnTerminate { refreshing.set(false) }
+            .subscribe()
+    }
 
     val navigateToWelcomeCommand = command {
         rocketStore.getList()
@@ -27,5 +51,10 @@ class RocketsViewModel(
     val navigateToLaunchesCommand = command<Rocket> {
         rocketAction.select(it)
             .subscribe { navigator.navigateTo(RocketsFragmentDirections.navigateToLaunches()) }
+    }
+
+    val filterInactiveRocketsCommand = command {
+        val previousFilterState = filterInactiveRocketsProcessor.value ?: false
+        filterInactiveRocketsProcessor.onNext(!previousFilterState)
     }
 }
