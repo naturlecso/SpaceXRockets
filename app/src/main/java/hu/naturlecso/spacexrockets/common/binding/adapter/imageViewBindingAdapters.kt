4dd41e567d0581429836b1ef1083c5c98de37a5c
Jozsef Szilvasi <jozsef.szilvasi@gmail.com>
2019-09-13 15:26:00 +0200

    Replace picasso with coil

--- app/src/main/java/hu/naturlecso/spacexrockets/common/binding/adapter/imageViewBindingAdapters.kt
+++ app/src/main/java/hu/naturlecso/spacexrockets/common/binding/adapter/imageViewBindingAdapters.kt
@@ -3,16 +3,16 @@ package hu.naturlecso.spacexrockets.common.binding.adapter
 import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
-import com.squareup.picasso.Picasso
+import coil.api.load
 
 @BindingAdapter(value = ["url", "placeholder", "size"])
 fun ImageView.bindUrl(url: String?, placeholder: Drawable?, size: Float) {
     url ?: return
     placeholder ?: return
 
-    Picasso.get()
-            .load(url)
-            .resize(size.toInt(), size.toInt())
-            .placeholder(placeholder)
-            .into(this)
+    load(url) {
+        crossfade(true)
+        placeholder(placeholder)
+        size(size.toInt())
+    }
 }
